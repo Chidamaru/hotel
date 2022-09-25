c53b3fba51a410a8111b6513625f3441065aa555
下高原汐理 <shimo.shiori@kakougenshiorinoMacBook-Air.local>
2022-09-25 22:45:55 +0900

    予約機能でエラー

--- app/controllers/concerns/reservations_controller.rb
+++ app/controllers/concerns/reservations_controller.rb
@@ -1,6 +1,6 @@
 class ReservationsController < ApplicationController
   def create
-    @reservation = current_login.reservations.create(reservation_params)
+    @reservation = current_user.reservations.create(reservation_params)
     redirect_to root_path notice:"予約が完了しました"
   end
 
@@ -10,11 +10,11 @@ class ReservationsController < ApplicationController
    params.require(:reservation).permit(:start_date, :end_date, :count, :price, :post_id)
   end 
   def index
-    @reservations = current_login.reservations.all
+    @reservations = current_user.reservations.all
   end
 
     def lend
-    @posts = current_login.posts
+    @posts = current_user.posts
     end 
 
 end
